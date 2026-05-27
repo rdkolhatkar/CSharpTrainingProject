d2e4f71cda6674c211b45b003f81a65b67d6fd5f
Ratnakar <ratnakarkolhatkar@gmail.com>
2026-05-27 21:12:57 +0530

    Imprtant Code Changes

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using CSharpTrainingProject.src;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // To import 'TutorialOne.Run()' We have added 'using CSharpTrainingProject.src'
+        // Here 'CSharpTrainingProject' is the name of this project and 'src' is the folder where we have the '01_TutorialOne.cs' file.
         TutorialOne.Run();
         TutorialTwo.Run();
     }
--- src/01_TutorialOne.cs
+++ src/01_TutorialOne.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Diagnostics;
 
-public class TutorialOne
+/* 
+* Just like in Java we have the 'import' keyword to import the packages, similarly in C# we have 'using' keyword
+* 'namespace' in C# is just like a package in Java.
+* If we have to import the code from this "01_TutorialOne.cs" file into some another class then we have to import this namespace.
+*/
+namespace CSharpTrainingProject.src
 {
-    public static void Run()
+
+    public class TutorialOne
     {
-        Console.WriteLine("Tutorial One");
+        public static void Run()
+        {
+            Console.WriteLine("Tutorial One"); // Basic Print Statement in C# using Console class in System namespace
+            System.Console.WriteLine("This is another way to Print Tutorial One"); // Basic Print Statement called using System namespace
+
+            // Basic Print Statement in C# using Debug class
+            Debug.WriteLine("Printing Tutorial One with Debug class");
+            // Debug class is present inside 'System.Diagnostics' namespace
+            System.Diagnostics.Debug.WriteLine("Printing Tutorial One with System.Diagnostics.Debug");
+            // Important Note: Print Statements written with 'Debug or System.Diagnostics.Debug' will not be printed in Windows console or OS console, it will get printed in debug console of visual studio.
+        }
     }
+
 }
